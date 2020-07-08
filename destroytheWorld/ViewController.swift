c0c7350c7af844829c31b79ac465654d3425aa0e
amititme68 <amititme68@gmail.com>
2020-07-08 22:12:45 +0530

    hello world

---
+++ destroytheWorld/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  destroytheWorld
+//
+//  Created by Amit Kumar on 08/07/20.
+//  Copyright © 2020 Amit Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        print("Hello World!")
+    }
+
+
+}
+
